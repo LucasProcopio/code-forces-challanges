f3934e6fb1d033b61101904b1e539be8142875c5
Lucas Henrique Procopio <procmode.dev@gmail.com>
2020-04-14 20:09:46 -0300

    :tada: Solved sum of parts and elephant

---
+++ challange_sum.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, a, n) for (int i = a; i < n; i++)
+#define repe(i, a, n) for (int i = a; i <= n; i++)
+#define pb push_back
+typedef vector<int> VI;
+//header
+
+// INCLUDE HEADER FILES NEEDED BY YOUR PROGRAM
+// SOME LIBRARY FUNCTIONALITY MAY BE RESTRICTED
+// DEFINE ANY FUNCTION NEEDED
+// FUNCTION SIGNATURE BEGINS, THIS FUNCTION IS REQUIRED
+int minimumTime(int numOfParts, vector<int> parts)
+{
+  VI sums;
+  int ans = 0, sum = 0, best = 0;
+  sort(parts.begin(), parts.end());
+  for (int i = 0; i < numOfParts - 1; i++)
+  {
+    sum = max(parts[i], sum + parts[i]);
+    best = max(best, sum);
+    sums.pb(best + parts[i + 1]);
+  }
+
+  rep(i, 0, sums.size())
+  {
+    ans += sums[i];
+  }
+  return ans;
+}
+
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int n, partn;
+  VI v;
+  cin >> n;
+  rep(i, 0, n)
+  {
+    cin >> partn;
+    v.pb(partn);
+  }
+  cout << minimumTime(n, v);
+
+  return 0;
+}
---
+++ elephant.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, a, n) for (int i = a; i < n; i++)
+#define repe(i, a, n) for (int i = a; i <= n; i++)
+#define pb push_back
+typedef vector<int> VI;
+//header
+
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int x;
+  cin >> x;
+  cout << (x + 4) / 5 << '\n';
+  return 0;
+}
